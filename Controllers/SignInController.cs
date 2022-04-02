1197667cd59c97298ff064179f281b4e1d6451eb
Djhvbrc <djhvbrcer228@gmail.com>
2022-04-02 21:04:02 +0300

    ToDoListFiles

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class SignInController : Controller
+    {
+        ToDoListTYEntities context = new ToDoListTYEntities();
+        // GET: SignIn
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Users model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = null;
+                using (context)
+                {
+                    user = context.Users.FirstOrDefault(u => u.login == model.login && u.password == model.password);
+
+                    if (user != null)
+                    {
+                        UserID.ID = context.Users.Where(b => b.login == model.login && b.password == model.password).Select(b => b.id).FirstOrDefault();
+                        FormsAuthentication.SetAuthCookie(model.login, true);
+                        return RedirectToAction("Index", "ToDoList");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                    }
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Users model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = null;
+                using (ToDoListTYEntities db = new ToDoListTYEntities())
+                {
+                    user = db.Users.FirstOrDefault(u => u.login == model.login);
+                }
+                if (user == null)
+                {
+                    using (ToDoListTYEntities db = new ToDoListTYEntities())
+                    {
+                        db.Users.Add(new Users { login = model.login, password = model.password});
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.login == model.login && u.password == model.password).FirstOrDefault();
+                    }
+                    if (user != null)
+                    {
+                        return RedirectToAction("Login", "SignIn");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Controllers/ToDoListController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        ToDoListTYEntities context = new ToDoListTYEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var lists = context.Lists.Where(b => b.id_user == UserID.ID).ToList();
+            var madelists = context.MadeLists.Where(b => b.id_user == UserID.ID).ToList();
+
+            IndexVM model = new IndexVM();
+            model.lists = lists;
+            model.madeLists = madelists;
+            return View(model);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Lists newList)
+        {
+
+            if (ModelState.IsValid)
+            {
+                newList.id_user = UserID.ID;
+                context.Lists.Add(newList);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(newList);
+            }
+        }
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            // Get movie to delete
+            var listToDelete = context.Lists.First(l => l.id == id);
+
+            // Delete 
+            context.Lists.Remove(listToDelete);
+            context.SaveChanges();
+
+            // Show Index view
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult Made(int id)
+        {
+            // Get movie to delete
+            var listToDelete = context.Lists.First(l => l.id == id);
+            
+            MadeLists madeLists = new MadeLists();
+            madeLists.id_user = UserID.ID;
+
+            madeLists.name = listToDelete.name;
+            madeLists.id_user = listToDelete.id_user;
+
+            context.MadeLists.Add(madeLists);
+            
+            context.Lists.Remove(listToDelete);
+            context.SaveChanges();
+
+            // Show Index view
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
